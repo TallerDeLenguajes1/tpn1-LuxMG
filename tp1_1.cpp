b0af7724a02011c055467578fc92730453252144
Luz <lucecitagiuliani@gmail.com>
2020-03-26 17:02:29 -0300

    Completado el punto 1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,1 +1,15 @@
-#
+#include <stdio.h>
+
+int main(){
+	int variable = 1;
+	int *pVar;
+	pVar = &variable;
+	
+	printf("Contenido del puntero: %d",*pVar);
+	printf("Direccion de memoria almacenada por el puntero: %p",pVar);
+	printf("Direccion de memoria de la variable: %p",&variable);
+	printf("Direccion de memoria del puntero: %p",&pVar);
+	printf("Tamaño de memoria usado por la variable: %d bytes",sizeof(variable));
+	
+	return 0;
+}
