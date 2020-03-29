9ef929ba69e2af5d26a9ce1392b46a48f4129022
Luz <lucecitagiuliani@gmail.com>
2020-03-29 14:15:39 -0300

    Ordenado del main de mejor forma

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -5,45 +5,22 @@ void cuadradoV(int a, int *cuadrado);
 void mostrar(int var);
 void invertir(int *a, int *b);
 void ordenar (int *a, int *b);
+void probarFunc();
 
 //---------------------------------------
 int main(){
-	/* Todo esto sirvió para probar que el código de las funciones funcionara correctamente
-	int numero = 3, cuad, numero2 = 5;
+	//probarFunc();
 	
-	cuad = cuadrado(numero);
-	printf("Usando la funcion cuadrado: %d\n",cuad);
-	printf("\n");
-	
-	cuad = 0;
-	cuadradoV(numero,&cuad);
-	printf("Usando la funcion cuadrado void: %d\n",cuad);
-	printf("\n");
-	
-	mostrar(numero);
-	printf("\n");
-	
-	printf("Antes de invertir:\nNumero a: %d\nNumero b: %d\n",numero,numero2);
-	printf("\n");
-	invertir(&numero,&numero2);
-	printf("Despues de invertir:\nNumero a: %d\nNumero b: %d\n",numero,numero2);
-	printf("\n");
-	
-	numero = 2; numero2 = -9;
-	printf("Antes de ordenar:\nNumero 1: %d\nNumero 2: %d\n",numero,numero2);
-	printf("\n");
-	ordenar(&numero,&numero2);
-	printf("Despues de ordenar:\nNumero 1(menor): %d\nNumero 2(mayor): %d\n",numero,numero2);
-	printf("\n");
-	*/
-	
+	printf("Este programa ordena numeros y devuelve primero el menor y segundo el mayor.\n");
 	printf("Ingrese pares de valores separados por un espacio, si desea finalizar, ingrese ambos como 0\n\n");
 	int a,b;
 	
 	do{
 		scanf("%d %d",&a,&b);
-		ordenar(&a,&b);
-		printf("%d %d\n\n",a,b);
+		if (a != 0 && b != 0){
+			ordenar(&a,&b);
+			printf("%d %d\n\n",a,b);
+		}
 	}while(a != 0 && b != 0); //coloqué una condicion simplemente para que hubiese una forma de terminar el programa
 	
 	return 0;
@@ -81,4 +58,38 @@ void ordenar(int *a, int *b){
 	if(*a > *b){
 		invertir(a,b);
 	}
+}
+
+// Para probar que el código de las funciones funcionara correctamente
+void probarFunc(){
+	int numero, cuad, numero2;
+	
+	printf("Ingrese un numero: "); scanf("%d",&numero);
+	printf("\n");
+	cuad = cuadrado(numero);
+	printf("Usando la funcion cuadrado: %d\n",cuad);
+	printf("\n");
+	
+	cuad = 0;
+	cuadradoV(numero,&cuad);
+	printf("Usando la funcion cuadrado void: %d\n",cuad);
+	printf("\n");
+	
+	mostrar(numero);
+	printf("\n");
+	
+	printf("Ingrese dos numeros: "); scanf("%d %d",&numero,&numero2);
+	printf("\n");
+	printf("Antes de invertir:\nNumero a: %d\nNumero b: %d\n",numero,numero2);
+	printf("\n");
+	invertir(&numero,&numero2);
+	printf("Despues de invertir:\nNumero a: %d\nNumero b: %d\n",numero,numero2);
+	printf("\n");
+	
+	printf("Ingrese dos numeros: "); scanf("%d %d",&numero,&numero2);
+	printf("Antes de ordenar:\nNumero 1: %d\nNumero 2: %d\n",numero,numero2);
+	printf("\n");
+	ordenar(&numero,&numero2);
+	printf("Despues de ordenar:\nNumero 1(menor): %d\nNumero 2(mayor): %d\n",numero,numero2);
+	printf("\n");
 }
